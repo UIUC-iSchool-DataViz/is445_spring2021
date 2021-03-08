0fabadba3f3004a1015d0e1bb77a104689cfe048
Jill Naiman <jnaiman@gmail.com>
2021-03-07 19:54:56 -0600

    Auto-commit: Sun Mar  7 19:54:56 CST 2021

--- _site/auto_commit.sh
+++ _site/auto_commit.sh
@@ -1,7 +1,6 @@
-auto_commit.sh
-=======================================
 #!/bin/bash
 MESSAGE="Auto-commit: $(date)"
 REPO_PATH="/Users/jillnaiman/is445_spring2021"
-git -C "$REPO_PATH" add -A
-git -C "$REPO_PATH" commit -m "$MESSAGE"
+cd $REPO_PATH
+git add -A
+git commit -m "$MESSAGE"
