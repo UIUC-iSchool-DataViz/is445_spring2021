0a9c7604510271cb4b8c170fba6ff38454a1103d
Jill Naiman <jnaiman@gmail.com>
2021-04-12 11:15:25 -0500

    Auto-commit: Mon Apr 12 11:15:25 CDT 2021

--- week12/inClass/components/histogram-custom-d3-component.js
+++ week12/inClass/components/histogram-custom-d3-component.js
@@ -2,7 +2,11 @@ const React = require('react'); // javascript library for interaction
 const D3Component = require('idyll-d3-component'); // to interface with d3.js
 const d3 = require('d3'); // to use the d3 functions
 
-const size = 500; // setting the base size of the canvas
+// const size = 500; // setting the base size of the canvas
+
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    width = 1200 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
 // this class name matches with the call in the index.idyll file
 class HistogramCustomD3Component extends D3Component {
