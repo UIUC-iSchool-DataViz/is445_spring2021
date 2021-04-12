b0c5497f65d06153d1c7f81f4a7a50a4e838491d
Jill Naiman <jnaiman@gmail.com>
2021-04-12 10:47:16 -0500

    Auto-commit: Mon Apr 12 10:47:16 CDT 2021

--- _site/week12/inClass/components/custom-d3-component.js
+++ _site/week12/inClass/components/custom-d3-component.js
@@ -6,28 +6,29 @@ const size = 600; // setting the base size of the canvas
 
 // this class name matches with the call in the index.idyll file
 class CustomD3Component extends D3Component {
-  initialize(node, props) {
-    const svg = (this.svg = d3.select(node).append('svg'));
-    svg
-      .attr('viewBox', `0 0 ${size} ${size}`)
-      .style('width', '100%')
-      .style('height', 'auto');
-
-    svg
-      .append('circle')
-      .attr('r', 20)
-      .attr('cx', Math.random() * size)
-      .attr('cy', Math.random() * size);
-  }
-
-//  update(props, oldProps) {
-//    this.svg
-//      .selectAll('circle')
-//      .transition()
-//      .duration(750)
+    
+//  initialize(node, props) {
+//    const svg = (this.svg = d3.select(node).append('svg'));
+//    svg
+//      .attr('viewBox', `0 0 ${size} ${size}`)
+//      .style('width', '100%')
+//      .style('height', 'auto');
+//
+//    svg
+//      .append('circle')
+//      .attr('r', 20)
 //      .attr('cx', Math.random() * size)
 //      .attr('cy', Math.random() * size);
 //  }
+
+  update(props, oldProps) { // this "update" function is needed to change the plot
+    this.svg
+      .selectAll('circle')
+      .transition()
+      .duration(750)
+      .attr('cx', Math.random() * size)
+      .attr('cy', Math.random() * size);
+  }
 }
 
 module.exports = CustomD3Component; // this name is the same as the
