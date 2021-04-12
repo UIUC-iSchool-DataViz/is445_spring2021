7b331dcdc2bf0860dda8d6ba27810ed98fb08cf4
Jill Naiman <jnaiman@gmail.com>
2021-04-12 10:47:48 -0500

    Auto-commit: Mon Apr 12 10:47:48 CDT 2021

--- _site/week12/inClass/components/custom-d3-component.js
+++ _site/week12/inClass/components/custom-d3-component.js
@@ -7,19 +7,19 @@ const size = 600; // setting the base size of the canvas
 // this class name matches with the call in the index.idyll file
 class CustomD3Component extends D3Component {
     
-//  initialize(node, props) {
-//    const svg = (this.svg = d3.select(node).append('svg'));
-//    svg
-//      .attr('viewBox', `0 0 ${size} ${size}`)
-//      .style('width', '100%')
-//      .style('height', 'auto');
-//
-//    svg
-//      .append('circle')
-//      .attr('r', 20)
-//      .attr('cx', Math.random() * size)
-//      .attr('cy', Math.random() * size);
-//  }
+  initialize(node, props) { // "initialize" sets up initial canvas
+    const svg = (this.svg = d3.select(node).append('svg'));
+    svg
+      .attr('viewBox', `0 0 ${size} ${size}`)
+      .style('width', '100%')
+      .style('height', 'auto');
+
+    svg
+      .append('circle')
+      .attr('r', 20)
+      .attr('cx', Math.random() * size)
+      .attr('cy', Math.random() * size);
+  }
 
   update(props, oldProps) { // this "update" function is needed to change the plot
     this.svg
